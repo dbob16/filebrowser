3e7dc8d58d9fd843edc3fac5a4f8c6d6367f89df
Dilan Gilluly <gillulyd@gmail.com>
2025-02-19 12:05:13 -0500

    new tab for files

--- app.py
+++ app.py
@@ -17,12 +17,11 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def browse(request:Request, r_path:str=""):
-    full_path = os.path.join(filepath, r_path)
-    print(full_path)
+    full_path = os.path.join(filepath, r_path)    
     if os.path.isdir(full_path):
         dir_list = os.listdir(full_path)
         dir_list.sort()
-        contents = [(item, os.path.join(r_path, item)) for item in dir_list]
+        contents = [(item, os.path.join(r_path, item), os.path.isfile(os.path.join(full_path, item))) for item in dir_list]
         return templates.TemplateResponse(request=request, name="browse.html", context={"reponame": reponame, "path": r_path, "contents": contents})
     else:
         return FileResponse(full_path)
